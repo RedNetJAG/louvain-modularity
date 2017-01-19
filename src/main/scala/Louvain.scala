503459ed725acc944daf6daf685c97a547c31315
Jose <rednet@openmailbox.org>
2017-01-19 09:49:33 +0100

    Delete import org.apache.spark.Logging
    To make it works with spark versions above 1.5.2

--- src/main/scala/Louvain.scala
+++ src/main/scala/Louvain.scala
@@ -10,7 +10,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
 import org.apache.spark.graphx._
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.{Logging, SparkContext}
+import org.apache.spark.SparkContext
 
 class Louvain() extends Serializable{
   def getEdgeRDD(sc: SparkContext, conf: LouvainConfig, typeConversionMethod: String => Long = _.toLong): RDD[Edge[Long]] = {
